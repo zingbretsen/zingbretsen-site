71c55fc9931db80642e842438d2ab67ec24d7412
Zach Ingbretsen <zach@zingbretsen.com>
2020-09-05 13:49:38 -0400

    Adds typography defaults

---
+++ src/utils/typography.js
@@ -0,0 +1,5 @@
+import Typography from 'typography';
+import parnassusTheme from 'typography-theme-parnassus';
+const typography = new Typography(parnassusTheme);
+export const { scale, rhythm, options } = typography;
+export default typography;
