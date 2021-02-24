c568b9a90884e243ea2bc800d196a39fe9b9e7dc
Zach Ingbretsen <zach@zingbretsen.com>
2021-02-24 07:54:03 -0500

    Switches from remark to mdx
    This will allow me to use jsx in markdown

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { Link } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
 import Layout from "../components/layout";
 
 export default function Template({ data }) {
-  const { markdownRemark } = data; // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark;
+  const { mdx } = data;
+  const { frontmatter, body } = mdx;
   return (
     <Layout title="Zach Overflow Blog" description={frontmatter.title}>
       <h1 className="blogtitle">{frontmatter.title}</h1>
       <p className="blogdate">{frontmatter.date}</p>
       <div className="blog-post">
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+        <MDXRenderer className="blog-post-content">{body}</MDXRenderer>
         <Link to="/blog">Go back to the blog</Link>
       </div>
     </Layout>
@@ -24,8 +22,8 @@ export default function Template({ data }) {
 
 export const pageQuery = graphql`
   query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
-      html
+    mdx(frontmatter: { path: { eq: $path } }) {
+      body
       frontmatter {
         date(formatString: "MMMM DD, YYYY")
         path
