bdf673940397d4644ecd1457e1b2271256eade6b
Zach Ingbretsen <zach@zingbretsen.com>
2021-02-24 08:46:01 -0500

    Puts blog posts in <article> tag

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -10,12 +10,12 @@ export default function Template({ data }) {
   const { frontmatter, body } = mdx;
   return (
     <Layout title="Zach Overflow Blog" description={frontmatter.title}>
-      <h1 className="blogtitle">{frontmatter.title}</h1>
-      <p className="blogdate">{frontmatter.date}</p>
-      <div className="blog-post">
+      <article>
+        <h1 className="blogtitle">{frontmatter.title}</h1>
+        <p className="blogdate">{frontmatter.date}</p>
         <MDXRenderer className="blog-post-content">{body}</MDXRenderer>
-        <Link to="/blog">Go back to the blog</Link>
-      </div>
+      </article>
+      <Link to="/blog">Go back to the blog</Link>
     </Layout>
   );
 }
