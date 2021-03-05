5628c0c34b82fbc2d8ec837cf727eb3e30dad9e6
Zach Ingbretsen <zach@zingbretsen.com>
2021-03-05 08:06:51 -0500

    Layout tweaks (#13)
    * Removes unused imgs, changes whoami graphql query & layout
    
    * Removes unnecessary imports in index.js
    
    * Minor addition to .gitignore
    
    * CSS tweaks
    
    * Moves whoami content to home page
    
    Assorted tweaks to css for better centering/sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,62 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const IndexPage = () => {
+  const { allImageSharp } = useStaticQuery(
+    graphql`
+      query MyQuery {
+        allImageSharp(filter: { fluid: { originalName: { in: ["zach_crop.jpg"] } } }) {
+          edges {
+            node {
+              fluid {
+                src
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+
+  const imgs = allImageSharp.edges;
+
   return (
     <Layout title="Home">
+      <h1>&gt; whoami</h1>
       <p>
-        This site is being developed with Gatsby/React. There's no backend server! Blog posts will
-        be generated from markdown documents.
+        Professional nerd, foodie, and photographer. I am a data scientist with a passion for
+        problem solving, currently working at the intersection of <b>Data Science</b>, <b>DevOps</b>
+        , and <b>App Development</b>.
+      </p>
+      <img
+        alt="I'm this guy. Picture of me."
+        src={imgs[0].node.fluid.src}
+        className="whoami-image"
+      />
+      <p>
+        I started my journey into programming and data analysis as an undergrad at Dartmouth
+        studying social neuroscience. As a new member of my lab, I was tasked with manually cleaning
+        up our data files in Excel (things like dropping unnecessary columns, re-ordering columns,
+        filling blank cells). This was before Pandas or the Tidyverse, but I still felt like there
+        was a better way.
       </p>
       <p>
-        My first series of blog posts will be my "Superpowers" series: How Vim, Git, and Docker give
-        you superpowers! After that, I will do deeper dives into those topics, as well as Python, R,
-        JavaScript, and cloud infrastructure.
+        I automated the manual work of pre-processing our data, and went on to be the primary
+        developer/tech support for the lab. I went on to do fMRI and behavioral data analysis with
+        Python and Matlab. I stayed in the field after graduating as a lab manager, first at
+        Dartmouth and then at Harvard.
       </p>
-      <p>If any of that interests you, please check back again soon!</p>
+      <p>
+        I continue to have a passion for <b>automation</b> and <b>data</b> <b>analysis</b>. I
+        believe that systems should be set up so that the best way to do something is also the easy
+        way to do it. <b>Python</b> was the first programming language I loved, and <b>R</b> has
+        become a new favorite.
+      </p>
+      <p>
+        I believe that <b>Vim</b>, <b>Git</b>, and <b>Docker</b> give you superpowers.
+      </p>
     </Layout>
   );
 };
