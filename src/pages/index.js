6868b6befe5292f841ee193e05475554756653e7
Zach Ingbretsen <zach@zingbretsen.com>
2021-11-13 09:16:21 -0500

    About me update (#26)

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,22 +37,16 @@ const IndexPage = () => {
       />
       <p>
         I started my journey into programming and data analysis as an undergrad at Dartmouth
-        studying social neuroscience. As a new member of my lab, I was tasked with manually cleaning
-        up our data files in Excel (things like dropping unnecessary columns, re-ordering columns,
-        filling blank cells). This was before Pandas or the Tidyverse, but I still felt like there
-        was a better way.
-      </p>
-      <p>
-        I automated the manual work of pre-processing our data, and went on to be the primary
-        developer/tech support for the lab. I went on to do fMRI and behavioral data analysis with
+        studying social neuroscience. As a new member of my lab, I automated the manual work of pre-processing our data.
+        I went on to do fMRI and behavioral data analysis with
         Python and Matlab. I stayed in the field after graduating as a lab manager, first at
         Dartmouth and then at Harvard.
       </p>
       <p>
+        I am now a data scientist, working on a wide variety of problems.
         I continue to have a passion for <b>automation</b> and <b>data</b> <b>analysis</b>. I
         believe that systems should be set up so that the best way to do something is also the easy
-        way to do it. <b>Python</b> was the first programming language I loved, and <b>R</b> has
-        become a new favorite.
+        way to do it.
       </p>
       <p>
         I believe that <b>Vim</b>, <b>Git</b>, and <b>Docker</b> give you superpowers.
