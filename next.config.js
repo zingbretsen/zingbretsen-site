2f72582553452aa3a6ecd54e8606cb1688e2560d
Zach Ingbretsen <zach@zingbretsen.com>
2022-11-24 23:27:30 -0500

    Modifies next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: ['remark-frontmatter'],
-    rehypePlugins: []
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: '@mdx-js/react'
-  }
-});
-module.exports = withMDX({
+module.exports = {
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  reactStrictMode: true
-});
+  reactStrictMode: true,
+  distDir: 'build'
+};
