c38e1bcea8e634c4d8a06f6091aba4a2b3e4e0fd
Zach Ingbretsen <zach@zingbretsen.com>
2022-11-25 22:29:56 -0500

    Adds meta charSet for a11y

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -40,6 +40,7 @@ const Layout = ({ children, banner, meta }) => {
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
       </Head>
       <Header />
       <div className={banner_class}>{banner}</div>
