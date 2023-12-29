9036a3d505e093a570adffd4ebcf68c24cb36f90
Zach <gitlab@zingbretsen.com>
2023-12-29 15:29:06 -0500

    formatting

--- pages/api/slackbot.js
+++ pages/api/slackbot.js
@@ -2,7 +2,7 @@ import { App } from '@slack/bolt';
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 app.command('/ping', async ({ ack, say }) => {
