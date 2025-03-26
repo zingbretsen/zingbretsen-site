0016fb3b5a6cfb57d0926796c579f541a0a32eff
Zach Ingbretsen <zach@zingbretsen.com>
2025-03-26 10:51:56 -0400

    Fix header layout for proper navigation display

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,18 +3,23 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <nav className="h-12 flex flex-row bg-[var(--light-gray)]">
-      <p className="brand m-2 text-center text-[var(--viridis-2)] p-2">Zach Ingbretsen</p>
-      <ul className="flex flex-row">
-        <li className="list-none m-2 text-center text-[var(--viridis-2)] p-2">
-          <Link href="/" className="hover:bg-[var(--lighter-gray)] hover:text-[var(--viridis-1)]">
+    <nav className="flex flex-col sm:flex-row items-center justify-between px-4 py-2 bg-[var(--light-gray)]">
+      <div className="text-xl font-semibold text-center text-[var(--viridis-2)]">
+        Zach Ingbretsen
+      </div>
+      <ul className="flex flex-row space-x-6 mt-2 sm:mt-0">
+        <li className="list-none text-center text-[var(--viridis-2)]">
+          <Link
+            href="/"
+            className="hover:bg-[var(--lighter-gray)] hover:text-[var(--viridis-1)] px-2 py-1 rounded"
+          >
             Home
           </Link>
         </li>
-        <li className="list-none m-2 text-center text-[var(--viridis-2)] p-2">
+        <li className="list-none text-center text-[var(--viridis-2)]">
           <Link
             href="/blog/"
-            className="hover:bg-[var(--lighter-gray)] hover:text-[var(--viridis-1)]"
+            className="hover:bg-[var(--lighter-gray)] hover:text-[var(--viridis-1)] px-2 py-1 rounded"
           >
             Blog
           </Link>
