63baefc6296d219d595c9728baa4e7483e7dccc8
Zach Ingbretsen <zach@zingbretsen.com>
2025-03-26 10:53:17 -0400

    keep nav bar all one line

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,10 +4,10 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <nav className="flex flex-col sm:flex-row items-center justify-between px-4 py-2 bg-[var(--light-gray)]">
-      <div className="text-xl font-semibold text-center text-[var(--viridis-2)]">
-        Zach Ingbretsen
-      </div>
       <ul className="flex flex-row space-x-6 mt-2 sm:mt-0">
+        <div className="text-xl font-semibold text-center text-[var(--viridis-2)]">
+          Zach Ingbretsen
+        </div>
         <li className="list-none text-center text-[var(--viridis-2)]">
           <Link
             href="/"
